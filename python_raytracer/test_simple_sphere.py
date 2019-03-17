0bdd8a55b5f733fb1bcbeb98da6956a3d211f36e
Christian Westerlund <ca.westerlund@gmail.com>
2019-03-17 18:59:51 +0200

    Add reflection

--- python_raytracer/test_simple_sphere.py
+++ python_raytracer/test_simple_sphere.py
@@ -6,8 +6,8 @@ from PIL import Image
 RED = Vec3(255, 0, 0)
 GREEN = Vec3(0, 255, 0)
 BLUE = Vec3(0, 0, 255)
-WHITE = Vec3(0,0,0)
-BLACK = Vec3(255, 255, 255)
+BLACK = Vec3(0,0,0)
+WHITE = Vec3(255, 255, 255)
 
 def form_image(data, height, width):
     image = Image.new('RGB', (height, width))
@@ -27,7 +27,7 @@ def scale(old_min, old_max, new_min, new_max, old_value):
     return int(new_value)
 
 def reflect(dir, normal):
-    return dir - 2 * (dir.normalize() * normal) * normal
+    return dir.normalize() - 2 * (dir.normalize() * normal) * normal
 
 def trace_ray(ray, depth):
     for obj in objects:
@@ -38,27 +38,24 @@ def trace_ray(ray, depth):
             N = obj.get_normal(point)
             ratio = max(0, N*V)
             color = ratio * obj.color
-            reflection = Ray(point + 0.001, reflect(ray.direction, N))
+            moved_point = point + N * 0.001
+            reflection = Ray(moved_point, reflect(ray.direction, N))
             if depth < 3:
-                rc = trace_ray(reflection, depth + 1)
-                color += rc
-                color = 0.7 * color
-                return color
+                color += 0.7 * trace_ray(reflection, depth + 1)
             return color
 
     # black
     return Vec3(0, 0, 0)
 
 if __name__ == "__main__":
-
     width = 500
     height = 500
-    sphere_origin = Vec3(250, 250, 0)
+    sphere_origin = Vec3(250, 250, 30)
     objects = []
     s_0 = Sphere(sphere_origin, 100, RED)
-    s_1 = Sphere(Vec3(100, 100, 0), 70, GREEN)
-    s_2 = Sphere(Vec3(400, 100, 0), 50, BLUE)
-    s_3 = Sphere(Vec3(250, 250, 100), 400, BLUE)
+    s_1 = Sphere(Vec3(100, 200, 10), 70, GREEN)
+    s_2 = Sphere(Vec3(400, 100, 20), 50, WHITE)
+    s_3 = Sphere(Vec3(500, 500, 100), 400, BLUE)
     objects.append(s_0)
     objects.append(s_1)
     objects.append(s_2)
@@ -74,6 +71,8 @@ if __name__ == "__main__":
             ray.direction.y = y
             color = trace_ray(ray, 0)
             data[x + y * width] = color.round_tuple()
+            if x % 400 == 0 and y % 100 == 0:
+                form_image(data, height, width)
 
 
     form_image(data, height, width)
